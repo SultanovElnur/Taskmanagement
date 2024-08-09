881dc0ac7277166b76b22025a5d7a171af456df7
elnur.sultanov <elnur.sultanov@cybernet.az>
2024-08-10 00:36:24 +0400

    added caching

--- src/main/java/com/sultanov/taskmanagement/controller/CommentController.java
+++ src/main/java/com/sultanov/taskmanagement/controller/CommentController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/tasks/{taskId}/comments")
+@RequestMapping("/tasks/{taskId}/comment")
 public class CommentController {
 
     private final CommentService commentService;
--- src/main/java/com/sultanov/taskmanagement/service/TaskService.java
+++ src/main/java/com/sultanov/taskmanagement/service/TaskService.java
@@ -9,6 +9,10 @@ import com.sultanov.taskmanagement.repository.TaskRepository;
 import com.sultanov.taskmanagement.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,6 +26,8 @@ public class TaskService {
     private final UserRepository userRepository;
     private final TaskMapper taskMapper;
 
+    @CacheEvict(value = "TaskCache", allEntries = true)
+    @CachePut(value = "TaskCache", key = "#result.id")
     public TaskDto createTask(TaskDto taskDTO) {
         Task task = taskMapper.toEntity(taskDTO);
         User author = userRepository.findByEmail(taskDTO.getAuthor().getEmail());
@@ -34,28 +40,40 @@ public class TaskService {
     }
 
     @Transactional
+    @CacheEvict(value = "TaskCache", allEntries = true)
+    @CachePut(value = "TaskCache", key = "#taskId")
     public TaskDto updateTask(Long taskId, TaskUpdateDto taskUpdateDto) {
         Task task = taskRepository.findById(taskId).orElseThrow(() -> new RuntimeException("Task not found"));
+        User currentUser = userRepository.findByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
+        if (!task.getAssignee().equals(currentUser)) {
+            throw new RuntimeException("You are not authorized to update this task.");
+        }
+
         taskMapper.updateFromDTO(taskUpdateDto, task);
         return taskMapper.toDto(taskRepository.save(task));
     }
 
+    @CacheEvict(value = "TaskCache", key = "#taskId")
     public void deleteTask(Long taskId) {
         taskRepository.deleteById(taskId);
     }
 
+    @Cacheable(value = "TaskCache", key = "#taskId")
     public TaskDto getTaskById(Long taskId) {
         return taskMapper.toDto(taskRepository.findById(taskId).orElseThrow(() -> new RuntimeException("Task not found")));
     }
 
+    @Cacheable(value = "TaskCache", key = "#authorId")
     public List<TaskDto> getTasksByAuthorId(Long authorId) {
         return taskRepository.findByAuthorId(authorId).stream().map(taskMapper::toDto).collect(Collectors.toList());
     }
 
+    @Cacheable(value = "TaskCache", key = "#assigneeId")
     public List<TaskDto> getTasksByAssigneeId(Long assigneeId) {
         return taskRepository.findByAssigneeId(assigneeId).stream().map(taskMapper::toDto).collect(Collectors.toList());
     }
 
+    @Cacheable(value = "TaskCache")
     public List<TaskDto> getAllTask() {
         return taskMapper.allToDto(taskRepository.findAll());
     }
